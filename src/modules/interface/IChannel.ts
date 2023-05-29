a43aae84edc78ba62b9d92a8bd484a5575528f24
Muhammad <hi@trader4.net>
2023-05-29 18:51:44 +0100

    [FIX] Fix mirror issues

--- src/modules/interface/IChannel.ts
+++ src/modules/interface/IChannel.ts
@@ -2,12 +2,14 @@ import { IResponse } from "./IResponse";
 import { IProject } from "./IProject";
 
 export interface IChannel extends IResponse {
-  uuid: string;
-  project?: IProject;
-  title: string;
-  description?: string;
-  logo?: string;
-  cover?: string;
-  is_public?: boolean;
-  status?: number;
+  results: {
+    uuid: string;
+    project?: IProject;
+    title: string;
+    description?: string;
+    logo?: string;
+    cover?: string;
+    is_public?: boolean;
+    status?: number;
+  }
 }
--- src/modules/interface/IChannelList.ts
+++ src/modules/interface/IChannelList.ts
@@ -1,13 +1,6 @@
 import {IResponse} from "./IResponse";
-import {IProject} from "./IProject";
+import {IChannel} from "./IChannel";
 
-export interface IChannel extends IResponse{
-    uuid: string;
-    project: IProject;
-    title: string;
-    description?: string;
-    logo?: string;
-    cover?: string;
-    is_public?: boolean;
-    status?: number;
+export interface IChannelList extends IResponse{
+    results: IChannel[]
 }
